5eaf804c2e25f3a7d3848d88a6e750a6ed4a5bc8
Isna Prasetyo <praseetyaa@gmail.com>
2023-09-28 01:04:09 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'qubiql' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'spandiv' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'bismillah*2020#' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
